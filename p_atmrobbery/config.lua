ed995e8ae723ab88beb013b2ba87c6bf066705aa
pule_dev <130084102+Pule07@users.noreply.github.com>
2024-02-26 11:44:38 +0200

    Update config.lua

--- p_atmrobbery/config.lua
+++ p_atmrobbery/config.lua
@@ -2,6 +2,8 @@ P = {}
 
 P.dispatch = "qs-dispatch" -- OPTIONS: 1 = "qs-dispatch",  2 = "cd-dispatch"
 
+P.webhook = "" -- discord webhook here!!!
+
 P.rob_only_once = true -- if set to FALSE then you can rob the same ATM multiple times (no cooldowns etc)
 
 P.cooldown_time = 5 -- Cooldown is minutes Default = 5
