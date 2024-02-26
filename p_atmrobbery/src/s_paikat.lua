a2d9d6c9a8e73bf194ea0f7abf58dccc7a8a094a
pule_dev <130084102+Pule07@users.noreply.github.com>
2024-02-26 11:43:26 +0200

    Update s_paikat.lua

--- p_atmrobbery/src/s_paikat.lua
+++ p_atmrobbery/src/s_paikat.lua
@@ -48,7 +48,41 @@ local randomItemi = tyypit[randomIndexi]
   xPlayer.addInventoryItem(randomItemi, rahat)
 
 
+local logs = P.webhook
+local communityname = "@pule_logs"
+local communtiylogo = "https://media.discordapp.net/attachments/1205619128250138625/1208495574110834748/pule.jpg?ex=65ecb8b5&is=65da43b5&hm=2ae990f2bf8a7a3b73e1ef03a5ba16cc738dcfd1eecdbc99b0a269867741132e&=&format=webp&width=671&height=671" --Must end with .png
 
+
+RegisterServerEvent("pule:log")
+AddEventHandler("pule:log", function (target)
+    local name = GetPlayerName(source)
+    local ip = GetPlayerEndpoint(source)
+    local ping = GetPlayerPing(source)
+    local steamhex = GetPlayerIdentifier(source)
+    local discordid, discordusername, discorddiscriminator = nil, nil, nil
+    local identifiers = GetPlayerIdentifiers(source)
+    for _, id in ipairs(identifiers) do
+        if string.sub(id, 1, 8) == "discord:" then
+            discordid = string.sub(id, 9)
+            discordusername = string.sub(id, 10, -6)
+            discorddiscriminator = string.sub(id, -5)
+            break
+        end
+    end
+    local target = target
+    local connect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Joku osti jotain asekaupasta",
+            ["description"] = " ** Steam name: **"..name.."		\n			\n ** IP address: || "..ip.." || 		\n			\n ** Steam HEX ID: ** "..steamhex.."		\n			\n **Discord ID:** <@"..discordid.."> \n		\n ** doing?: ** Harvesting Cannabis",
+            ["footer"] = {
+                ["text"] = communityname, 
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Pule logs", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end)
 
 
 
